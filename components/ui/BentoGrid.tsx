2ccffd6a87984f8f69c76b645040c2ca2f3d8a39
infimas <irfaninfinity007@gmail.com>
2024-07-02 23:22:54 +0530

    secondUpdate

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "irfaninfinity007@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
